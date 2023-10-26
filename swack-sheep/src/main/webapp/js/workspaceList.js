ef8225e73f0990985be5bd6599cc07f87ce22e92
s20222053 <s20222053@HCS19069.stdlan.hcs.ac.jp>
2023-10-26 11:52:42 +0900

    js

--- swack-sheep/src/main/webapp/js/workspaceList.js
+++ swack-sheep/src/main/webapp/js/workspaceList.js
@@ -3,7 +3,6 @@
  */
 
 function clickWorkspace(num1, num2) {
-	console.log(num1);
 	document.getElementById("workspaceName").value = num1;
 	document.getElementById("workspaceID").value = num2;
     document.getElementById("wSpan1").textContent  = document.getElementById("workspaceName").value;
